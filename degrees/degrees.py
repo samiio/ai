e413b9da01b2ea7e5e43f66f4918b9239cfdb543
samiio <sami.ali.96@outlook.com>
2020-12-31 13:02:22 +0400

    removed other iterations

--- degrees/degrees.py
+++ degrees/degrees.py
@@ -1,66 +1,24 @@
-import csv
+"""
+Using list instead of set
+Removed node = child assigment in shortestPath()
+"""
 import sys
 import time
+import sqlite3
 
 from util import Node, StackFrontier, QueueFrontier
 
-# Maps names to a set of corresponding person_ids
-names = {}
+# Connect to database
+db = sqlite3.connect("movies.db")
 
-# Maps person_ids to dictionary: name, birth, movies (set of movie_ids)
+# Map person id to set(movies)
 people = {}
 
-# Maps movie_ids to dictionary: title, year, stars (set of person_ids)
+# Map movie id to set(stars)
 movies = {}
 
-def loadData(directory):
-    """
-    Load data from CSV files into memory.
-    """
-    begin = time.time() # start find time
-    # Load people
-    with open(f"{directory}/people.csv", encoding="utf-8") as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            people[row["id"]] = {
-                "name": row["name"],
-                "birth": row["birth"],
-                "movies": set()
-            }
-            if row["name"].lower() not in names:
-                names[row["name"].lower()] = {row["id"]}
-            else:
-                names[row["name"].lower()].add(row["id"])
-
-    # Load movies
-    with open(f"{directory}/movies.csv", encoding="utf-8") as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            movies[row["id"]] = {
-                "title": row["title"],
-                "year": row["year"],
-                "stars": set()
-            }
-
-    # Load stars
-    with open(f"{directory}/stars.csv", encoding="utf-8") as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            try:
-                people[row["person_id"]]["movies"].add(row["movie_id"])
-                movies[row["movie_id"]]["stars"].add(row["person_id"])
-            except KeyError:
-                pass
-    end = time.time() # end find time
-    print(f"\nTime: {end - begin}")
-
-
 def main():
-    # Load data from files into memory
-    print("Loading data...")
-    loadData("D:\\Documents\\p-projects\\ai\\degrees\\large")
-    print("Data loaded.")
-
+    loadData()
     source = personIdForName(input("Name: "))
     if source is None:
         sys.exit("Person not found.")
@@ -68,19 +26,71 @@ def main():
     if target is None:
         sys.exit("Person not found.")
 
+    begin = time.time() # start find time
     path = shortestPath(source, target)
 
     if path is None:
         print("Not connected.")
     else:
+        queryPeople = "SELECT name FROM people WHERE id = ?"
+        queryMovies = "SELECT title FROM movies WHERE id = ?"
         degrees = len(path)
-        print(f"{degrees} degrees of separation.")
+        print(f"\n{degrees} degrees of separation.")
         path = [(None, source)] + path
         for i in range(degrees):
-            person1 = people[path[i][1]]["name"]
-            person2 = people[path[i + 1][1]]["name"]
-            movie = movies[path[i + 1][0]]["title"]
+            person1 = getNameFromId(queryPeople, path[i][1])
+            person2 = getNameFromId(queryPeople, path[i + 1][1])
+            movie = getNameFromId(queryMovies, path[i + 1][0])
             print(f"{i + 1}: {person1} and {person2} starred in {movie}")
+    end = time.time() # end find time
+    print(f"\nTime: {end - begin}")
+
+
+def loadData():
+    begin = time.time() # start find time
+    # Load people
+    pids = getIds("SELECT id FROM people")
+    for pid in pids:
+        people[pid] = { "movies": set() }
+
+    # Load movies
+    mids = getIds("SELECT id FROM movies")
+    for mid in mids:
+        movies[mid] = { "stars": set() }
+
+    # Load stars
+    myStars = getStars()
+    for star in myStars:
+        # star[0] = movieId, star[1] = personId
+        try:
+            people[star[1]]["movies"].add(star[0])
+            movies[star[0]]["stars"].add(star[1])
+        except KeyError:
+            pass
+    end = time.time() # end find time
+    print(f"\nTime: {end - begin}")
+
+
+def getIds(query):
+    """
+    Return a list of every id in a table.
+    """
+    db.row_factory = lambda cursor, row: row[0]
+    c = db.cursor()
+    ids = c.execute(query).fetchall()
+    c.close()
+    return ids
+
+
+def getStars():
+    """
+    Return set of (movie_id, person_id) pairs from stars table.
+    """
+    db.row_factory = None
+    c = db.cursor()
+    stars = c.execute("SELECT movie_id, person_id FROM stars").fetchall()
+    c.close()
+    return stars
 
 
 def shortestPath(source, target):
@@ -90,83 +100,106 @@ def shortestPath(source, target):
 
     If no possible path, returns None.
     """
-    # Keep track of explored nodes 
-    num_explored = 0
+    def rPath(node: Node):
+        if node.parent is None:
+            return []
+        return [(node.action, node.state)] + rPath(node.parent)
+
+    def path(node: Node):
+        return list(reversed(rPath(node)))
+
+    # Keep track of explored nodes
     explored = set()
 
     # Initialise node and add it to frontier
     start = Node(state=source, parent=None, action=None)
     frontier = QueueFrontier()
     frontier.add(start)
-
+    
     # Loop to perform breadth-first search
     while True:
 
-        # If frontier is empty, there is no solution 
+        # If frontier is empty, there is no solution
         if frontier.empty():
             return None
 
         # Remove node from frontier, this node will be considered
         node = frontier.remove()
-        num_explored += 1
         explored.add(node.state)
+        if node.state == target:
+            return path(node)
 
-        # Find neighbors (actors to which he can connect) of the actor
-        neighbors = nieghborsForPerson(node.state)
-        for movie, actor in neighbors:
-            if actor not in explored and not frontier.contains_state(actor):
-                child = Node(state=actor, parent=node, action=movie)
+        nodes = [
+            Node(state=n[1], parent=node, action=n[0])
+            for n in neighborsForPerson(node.state)
+            if n[1] not in explored
+        ]
 
-                # If node contains goal state, return list of tuples
-                if child.state == target:
-                    path = []
-                    node = child
-                    while node.parent is not None:
-                        path.append((node.action, node.state))
-                        node = node.parent
+        # Optimization: check for goal nodes before adding to frontier
+        goalNodes = (node for node in nodes if node.state == target)
+        anyGoal = next(goalNodes, False)
+        if anyGoal:
+            return path(anyGoal)
 
-                    path.reverse()
-                    return path
-
-                frontier.add(child)
+        [frontier.add(node) for node in nodes]
 
 
 def personIdForName(name):
     """
     Returns the IMDB id for a person's name, resolving ambiguities.
     """
-    person_ids = list(names.get(name.lower(), set()))
-    if len(person_ids) == 0:
+    c = db.cursor()
+    births = c.execute(
+        "SELECT id, name, birth FROM people WHERE name LIKE ?;", 
+        (name.lower(),)).fetchall()
+    c.close()
+    
+    # Map id to (name and birth)
+    personDict = {}
+    births = list(births)
+    for person in births:
+        personDict[person[0]] = person[1] + ", " + str(person[2])
+
+    # Return output depending on dictionary length
+    if len(personDict) == 0:
         return None
-    elif len(person_ids) > 1:
+    elif len(personDict) > 1:
         print(f"Which '{name}'?")
-        for person_id in person_ids:
-            person = people[person_id]
-            name = person["name"]
-            birth = person["birth"]
-            print(f"ID: {person_id}, Name: {name}, Birth: {birth}")
+        for pId, pName in personDict.items():
+            print(f"ID: {pId}, Name: {pName}")
         try:
-            person_id = input("Intended Person ID: ")
-            if person_id in person_ids:
-                return person_id
+            personId = input("Intended Person ID: ")
+            if personId in personDict.keys():
+                return personId
         except ValueError:
             pass
         return None
     else:
-        return person_ids[0]
+        return list(personDict)[0]
 
 
-def nieghborsForPerson(person_id):
+def neighborsForPerson(person_id):
     """
     Returns (movie_id, person_id) pairs for people
     who starred with a given person.
     """
     movie_ids = people[person_id]["movies"]
-    neighbors = set()
+    neighbors = []
     for movie_id in movie_ids:
         for person_id in movies[movie_id]["stars"]:
-            neighbors.add((movie_id, person_id))
+            neighbors.append((movie_id, person_id))
     return neighbors
+
+
+def getNameFromId(query, anId):
+    """
+    Return the name with id 'personId'.
+    """
+    db.row_factory = lambda cursor, row: row[0]
+    c = db.cursor()
+    name = c.execute(query, (anId,)).fetchall()
+    c.close()
+    return name[0]
 
 
 if __name__ == "__main__":
